2c985e79eef525b733ecae1d21152a260ea8c819
cory <wolfe.cory1@gmail.com>
2017-06-15 18:57:01 -0400

    add cancer command (emotes between characters)

--- bot.js
+++ bot.js
@@ -30,6 +30,29 @@ bot.on('message', function(user, userID, channelID, message, event) {
 			});
 			console.log("Ran loot split command on channel " + channelID + "for user " + user + ".");
 		}
+		if(split[0] == "cancer"){
+			var len = split.length;
+			var emote = split[len-1];
+			var prString = emote;
+			if(emote.charCodeAt(0) == 55356){
+			for(var i=1;i<len-1;i++){
+				for(var o=0;o<split[i].length;o++){
+					prString+=split[i].charAt(o) +emote;
+				}
+				prString+=" ";
+			}
+			}else{
+			for(var i=1;i<len-1;i++){
+				for(var o=0;o<split[i].length;o++){
+					prString+=" " + split[i].charAt(o)+ " " +emote;
+				}
+			}
+			}
+			bot.sendMessage({
+				to: channelID,
+				message: prString
+			});
+		}
 	}
 });
 
